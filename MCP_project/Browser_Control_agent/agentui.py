95a645a55feb2e65ff173b625d0d17f020797d24
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-03-07 20:31:25 +0530

    complete t agent intialization and complete it stucture of this project

--- MCP_project/Browser_Control_agent/agentui.py
+++ MCP_project/Browser_Control_agent/agentui.py
@@ -4,30 +4,30 @@ import os
 from textwrap import dedent
 
 from mcp_agent.app import MCPApp
+from mcp_agent.agents.agent import Agent
 from mcp_agent.workflows.llm.augmented_llm_google import GoogleAugmentedLLM
 from mcp_agent.workflows.llm.augmented_llm import RequestParams
 
-
-# page configuration
-st.page_config(
+# Page configuration
+st.set_page_config(
     page_title="Browser Control Agent",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-# title
+# Title
 st.title("Browser Control Agent")
 
-# description
+# Description
 st.markdown("""
-This is a simple browser control agent that can be used to control the browser.
+This is a simple browser control agent that can be used to control the browser using MCP.
 """)
 
 with st.sidebar:
     st.markdown("## Configuration")
     st.markdown("""
-                Navigations
+                ### Navigations
     """)
     st.markdown("**Interactions**")
     st.markdown("- click on mcp_ai_agents")
@@ -40,3 +40,47 @@ with st.sidebar:
 
     st.markdown("---")
     st.caption("Note: The agent uses Playwright to control a real browser.")
+
+query = st.text_area(
+    "Your Instruction to Control Browser",
+    placeholder="Enter your instruction here"
+)
+
+# Initialize the app and agent that can control your browser
+if 'initialized' not in st.session_state:
+    st.session_state.initialized = False
+    st.session_state.mcp_app = MCPApp(
+        name="MCPA_AGENT", description="Browser Control Agent")
+    st.session_state.mcp_context = None
+    st.session_state.mcp_agent = None
+    st.session_state.mcp_workflow = None
+    st.session_state.mcp_browser_agent = None
+    st.session_state.llm = None
+
+
+async def setup_agent():
+    if not st.session_state.initialized:
+        try:
+            st.session_state.mcp_context = st.session_state.mcp_app.run()
+            st.session_state.mcp_agent_app = await st.session_state.mcp_context.__aenter__()
+
+            # create and initialized the agent
+            st.session_state.mcp_browser_agent = Agent(
+                name="Browser Agent",
+                instruction="""
+                you are helpful web browsing assistant that can interact with websites using playwright
+                -Navigate to websites and perform browser actions like (click,scroll,enter,exit,type)
+                -extract information from websites and web pages
+                -take screenshots of websites and web pages
+                -provide concise summaries of web content using markdown format
+                -follow the instruction sequences to complete tasks
+                
+                Respond back with status code updates completing the command and execution of instructions
+                """,
+                server_names=["playwright"],
+            )
+
+            st.session_state.initialized = True
+
+        except Exception as e:
+            raise e
